e99e807133bbe951ee26732fa6a61ba3c23b72da
Elliott Pope <epope@bu.edu>
2022-08-03 16:30:10 -0400

    Adding SELECT handler

---
+++ src/handlers/select.rs
@@ -0,0 +1,54 @@
+use crate::handlers::{HandleCommand};
+use crate::server::{Command, Response, ResponseStatus, ParseError};
+use crate::util::Result;
+
+pub struct SelectHandler{}
+#[async_trait::async_trait]
+impl HandleCommand for SelectHandler {
+    fn name<'a>(&self) -> &'a str {
+        "SELECT"
+    }
+    async fn validate<'a>(&self, command: &'a Command) -> Result<()> {
+        if command.command() != self.name() {
+            return Ok(())
+        }
+        if command.num_args() < 1 {
+            return Err(Box::new(ParseError{}))
+        }
+        Ok(())
+    }
+    async fn handle<'a>(&self, command: &'a Command) -> Result<Response> {
+        Ok(Response::new(command.tag(), ResponseStatus::OK, command.command(), "completed.".to_string()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SelectHandler;
+    use crate::handlers::HandleCommand;
+    use crate::server::{Command, ResponseStatus, Response};
+    use crate::util::Result;
+
+    #[async_std::test]
+    async fn test_can_login() {
+        let select_handler = SelectHandler{};
+        let select_command = Command::new(
+            "a1".to_string(),
+            "SELECT".to_string(),
+            vec!["INBOX".to_string()],
+        );
+        let valid = select_handler.validate(&select_command).await;
+        assert_eq!(valid.is_ok(), true);
+        let response = select_handler.handle(&select_command).await;
+        select_success(response);
+    }
+
+    fn select_success(response: Result<Response>) {
+        assert_eq!(response.is_ok(), true);
+        let reply = response.unwrap();
+        assert_eq!(reply.tag(), "a1".to_string());
+        assert_eq!(reply.status(), ResponseStatus::OK);
+        assert_eq!(reply.command(), "SELECT".to_string());
+        assert_eq!(reply.message(), "completed.".to_string());
+    }
+}
